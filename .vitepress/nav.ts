7945d87b975e97852332f69c1ada4f3c32044536
jlc <2794810071@qq.com>
2025-03-02 14:25:09 +0800

    update：Ubuntu内容的添加

--- .vitepress/nav.ts
+++ .vitepress/nav.ts
@@ -32,6 +32,7 @@ export default [
             { text: 'Markdown', link: '/T_Markdown/基本概念' },
             { text: 'Git', link: '/T_Git/基本概念' },
             { text: 'VSCode', link: '/T_VSCode/基本概念' },
+            { text: 'Ubuntu', link: '/T_Ubuntu/基本概念' },
             { text: 'Docker', link: '/T_Docker/基本概念' },
         ]
     },
